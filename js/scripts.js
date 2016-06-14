272549aed46aa40aa2f668ec7e75ee0c800fb2d7
Leah Nelson and Tony Fabery <leah@>
2016-06-14 16:42:48 -0700

    mo betta

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,22 @@
 //====================BackEnd=====================
-var keanu = {
-          name: "Keanu",
-          value: 5
-};
-var warcraft = {
-          name: "Warcraft",
-          value: 12
-};
-var blackway = {
-          name: "Blackway",
-          value: 3
-};
-var hockney = {
-          name: "Hockney",
-          value: 5
-};
-var am = {
-          time: "AM",
-          value: -1
-};
-var noon = {
-          time: "Noon",
-          value: -1
-};
-var pm = {
-          time: "PM",
-          value: 1
-};
-var midnight = {
-          time: "Midnight",
-          value: 5
-};
-var child = {
-          age: "Child",
-          value: -1
-};
-var adult = {
-          age: "Adult",
-          value: 0
-};
-var senior = {
-          age: "Senior",
-          value: -1
-};
-var military = {
-          age: "Military",
-          value: -1
-};
+function Option(variable, value) {
+  this.variable = variable;
+  this.value = value;
+}
+
+var keanu = new Option("Keanu", 5);
+var warcraft = new Option("Warcraft", 12);
+var blackway = new Option("Blackway", 3);
+var hockney = new Option("Hockney", 5);
+var am = new Option("7:00 AM", -1);
+var noon = new Option("Noon", -1);
+var pm = new Option("8:00 PM", 1);
+var midnight = new Option("Midnight", 5);
+var child = new Option("Child", 0.95);
+var adult = new Option("Adult", 1);
+var senior = new Option("Senior", 0.5);
+var military = new Option("Military", 0.75);
+
 var nameArray = [keanu, warcraft, blackway, hockney];
 var timeArray = [am, noon, pm, midnight];
 var ageArray = [child, adult, senior, military];
@@ -58,36 +28,31 @@ function Ticket(name, time, age) {
 }
 
 Ticket.prototype.price = function() {
-  var namePrice = 0;
-  var agePrice = 0;
-  var timePrice = 0;
-
+  var price = [];
   for (i=0; i< nameArray.length; i++) {
-    if (this.name === nameArray[i].name) {
-      namePrice = nameArray[i].value;
+    if (this.name === nameArray[i].variable) {
+      price.push(nameArray[i].value);
     }
   };
 
   for (i=0; i< timeArray.length; i++) {
-    if (this.time === timeArray[i].time) {
-      timePrice = timeArray[i].value;
+    if (this.time === timeArray[i].variable) {
+      price.push(timeArray[i].value);
     }
   };
 
   for (i=0; i< ageArray.length; i++) {
-    if (this.age === ageArray[i].age) {
-      agePrice = ageArray[i].value;
+    if (this.age === ageArray[i].variable) {
+      price.push(ageArray[i].value);
     }
   };
 
-  return namePrice + timePrice + agePrice;
+  return (price[0] + price[1]) * price[2];
 }
-
-
-
 
 //====================FrontEnd====================
 $(function() {
+  var price;
   $('.form').submit(function(event) {
     event.preventDefault();
 
@@ -95,9 +60,22 @@ $(function() {
     var time = $("#time").val();
     var age = $("#age").val();
     var newTicket = new Ticket(name, time, age);
-
-    $('#result').text('$' + newTicket.price());
+    price = newTicket.price().toFixed(2);
+    $('#result').text('$' + price);
     $('#price-result').show();
 
+  });
+
+  // $('.pay').click(function() {
+  //   window.location.href = '../bank/index.html' + '#' + price + '#' + ;
+  // });
+
+  $('.account').submit(function(event) {
+    event.preventDefault();
+
+    var name = $('input#person').val();
+    var initial = $('input#initial').val();
+
+    window.location.href = '../bank/index.html' + '#' + price + '-' + name + '-' + initial;
   });
 });
